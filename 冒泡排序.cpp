4a7bc09333f10dcc31471dd9c7c075e51e0a5585
hanrandane <32233157+hanrandane@users.noreply.github.com>
2017-11-21 20:28:14 +0800

    补

--- 冒泡排序.cpp
+++ 冒泡排序.cpp
@@ -3,20 +3,21 @@ int main(){
 	int n;
 	scanf("%d",&n);
 	int a[n];
-	for(int b=0;b<n;b=b+1)
-		scanf("%d",&a[b]);
-	for(int i=0,j,h;i<n-1;i=i+1){
-		for(int j=i+1;j<=n-1;j=j+1){
-			if(a[i]>a[j]){
-				h=a[i];
-				a[i]=a[j];
-				a[j]=h;
-			}
-		} 
-		for(int b=0;b<n;b=b+1){
-	       printf("%d ",a[b]);
-
-		   }
-		printf("\n");
+	for(int i=0;i<=n-1;i=i+1){
+		scanf("%d",&a[i]);
 	}
-} 
+	for(int i=n-1;i>=1;i=i-1){
+		for(int j=i-1;j>=0;j=j-1){
+			if(a[i]<a[j]){
+				int zhong=a[i];
+				a[i]=a[j];
+				a[j]=zhong;
+			}
+	    }
+	   for(int k=0;k<=n-1;k=k+1){
+	      printf("%d ",a[k]);
+	   }
+	   printf("\n");
+	}
+	return 0;
+}
